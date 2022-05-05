139e2c54292043172754220b1d936645e624eff3
wmax351 <wmax351@gmail.com>
2022-05-05 08:13:59 -0600

    Update __init__.py

--- fixgw/plugins/grand_rapids_eis/__init__.py
+++ fixgw/plugins/grand_rapids_eis/__init__.py
@@ -47,10 +47,10 @@ class MainThread(threading.Thread):
                     print("bad frame")
                     continue
                 tach1 = int.from_bytes(s[0:2], "big")
-                cht1 = int((int.from_bytes(s[2:4], "big") - 32) * 5.0/9)
-                cht2 = int((int.from_bytes(s[4:6], "big") - 32) * 5.0/9)
-                egt1 = int((int.from_bytes(s[6:8], "big") - 32) * 5.0/9)
-                egt2 = int((int.from_bytes(s[8:10], "big") - 32) * 5.0/9)
+                cht1 = int((int.from_bytes(s[2:4], "big")))
+                cht2 = int((int.from_bytes(s[4:6], "big")))
+                egt1 = int((int.from_bytes(s[6:8], "big")))
+                egt2 = int((int.from_bytes(s[8:10], "big")))
                 airspeed = int.from_bytes(s[10:12], "big") #guess
                 altitude = int.from_bytes(s[12:14], "big")
                 volts = int.from_bytes(s[14:16], "big")/10.0
@@ -60,9 +60,9 @@ class MainThread(threading.Thread):
                 oat = s[20]
                 if (oat > 127):
                     oat -= 256
-                oat = int((oat - 32) * 5.0/9)
-                coolant = int((int.from_bytes(s[21:23], "big") - 32) * 5.0/9)
-                oilt = int((int.from_bytes(s[23:25], "big") - 32) * 5.0/9)
+                oat = int((oat))
+                coolant = int((int.from_bytes(s[21:23], "big")))
+                oilt = int((int.from_bytes(s[23:25], "big")))
                 oilp = s[25]
                 aux1 = int.from_bytes(s[26:28], "big")
                 aux2 = int.from_bytes(s[28:30], "big")
@@ -98,18 +98,18 @@ class MainThread(threading.Thread):
                     continue
 
                 tach1 = int.from_bytes(s[0:2], "big")
-                cht1 = int((int.from_bytes(s[2:4], "big") - 32) * 5.0/9)
-                cht2 = int((int.from_bytes(s[4:6], "big") - 32) * 5.0/9)
-                cht3 = int((int.from_bytes(s[6:8], "big") - 32) * 5.0/9)
-                cht4 = int((int.from_bytes(s[8:10], "big") - 32) * 5.0/9)
-                cht5 = int((int.from_bytes(s[10:12], "big") - 32) * 5.0/9)
-                cht6 = int((int.from_bytes(s[12:14], "big") - 32) * 5.0/9)
-                egt1 = int((int.from_bytes(s[14:16], "big") - 32) * 5.0/9)
-                egt2 = int((int.from_bytes(s[16:18], "big") - 32) * 5.0/9)
-                egt3 = int((int.from_bytes(s[18:20], "big") - 32) * 5.0/9)
-                egt4 = int((int.from_bytes(s[20:22], "big") - 32) * 5.0/9)
-                egt5 = int((int.from_bytes(s[22:24], "big") - 32) * 5.0/9)
-                egt6 = int((int.from_bytes(s[24:26], "big") - 32) * 5.0/9)
+                cht1 = int((int.from_bytes(s[2:4], "big")))
+                cht2 = int((int.from_bytes(s[4:6], "big")))
+                cht3 = int((int.from_bytes(s[6:8], "big")))
+                cht4 = int((int.from_bytes(s[8:10], "big")))
+                cht5 = int((int.from_bytes(s[10:12], "big")))
+                cht6 = int((int.from_bytes(s[12:14], "big")))
+                egt1 = int((int.from_bytes(s[14:16], "big")))
+                egt2 = int((int.from_bytes(s[16:18], "big")))
+                egt3 = int((int.from_bytes(s[18:20], "big")))
+                egt4 = int((int.from_bytes(s[20:22], "big")))
+                egt5 = int((int.from_bytes(s[22:24], "big")))
+                egt6 = int((int.from_bytes(s[24:26], "big")))
                 aux5 = int.from_bytes(s[26:28], "big")
                 aux6 = int.from_bytes(s[28:30], "big")
                 airspeed = int.from_bytes(s[30:32], "big")
@@ -122,14 +122,14 @@ class MainThread(threading.Thread):
                 oat = s[41]
                 if (oat > 127):
                     oat -= 256
-                oat = int((oat - 32) * 5.0/9)
-                oilt = int((int.from_bytes(s[42:44], "big") - 32) * 5.0/9)
+                oat = int((oat))
+                oilt = int((int.from_bytes(s[42:44], "big")))
                 oilp = s[44]
                 aux1 = int.from_bytes(s[45:47], "big")
                 aux2 = int.from_bytes(s[47:49], "big")
                 aux3 = int.from_bytes(s[49:51], "big")
                 aux4 = int.from_bytes(s[51:53], "big")
-                coolant = int((int.from_bytes(s[53:55], "big") - 32) * 5.0/9)
+                coolant = int((int.from_bytes(s[53:55], "big")))
                 engine_time = int.from_bytes(s[55:57], "big")/10.0
                 fuel_qty = int.from_bytes(s[57:59], "big")
                 hours = s[59]
@@ -165,7 +165,7 @@ class MainThread(threading.Thread):
                 self.parent.db_write("H2OT1", coolant)
                 # need to correct analogs
                 self.parent.db_write("ANLG1", aux1)
-                self.parent.db_write("ANLG2", aux2)
+                self.parent.db_write("FUELP1", aux2)
                 self.parent.db_write("ANLG3", aux3)
                 self.parent.db_write("ANLG4", aux4)
                 
